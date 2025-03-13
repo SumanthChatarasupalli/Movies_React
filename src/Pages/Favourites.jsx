930bf4b30c639340d321180e082bec972eb9d1a2
Sumanth Chatarasupalli <136967267+SumanthChatarasupalli@users.noreply.github.com>
2025-03-13 17:49:27 +0530

    Favourites Page is Created and Used

--- src/Pages/Favourites.jsx
+++ src/Pages/Favourites.jsx
@@ -20,7 +20,7 @@ function Favorites() {
 
   return (
     <div className="favorites-empty">
-      <h2>No Favorite Movies Yet</h2>
+      <h3>No Favorite Movies Yet</h3>
       <p>Start adding movies to your favorites and they will appear here!</p>
     </div>
   );
